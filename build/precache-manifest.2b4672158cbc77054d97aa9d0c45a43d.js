478d24f411b52e6688767438ab2758e38dbd388c
CorellCB <corellcb@gmail.com>
2020-01-15 20:22:37 -0500

    build

---
+++ build/precache-manifest.2b4672158cbc77054d97aa9d0c45a43d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5835a01ce3b3802a5711743eba35ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d486f1274ea8a06b20e6",
+    "url": "/static/css/main.61b5a5c5.chunk.css"
+  },
+  {
+    "revision": "fa40d41414ee0b9aebaa",
+    "url": "/static/js/2.88feffe3.chunk.js"
+  },
+  {
+    "revision": "d486f1274ea8a06b20e6",
+    "url": "/static/js/main.7589ba8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
